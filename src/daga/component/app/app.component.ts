d566fd9a1ee88a86855fa3843ff99bdd9c229b81
Pedro J. Molina <pjmolina@metadev.pro>
2026-05-28 11:42:01 +0200

    fix

--- src/daga/component/app/app.component.ts
+++ src/daga/component/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ProbComponent } from "../prob.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterModule, ProbComponent]
+  imports: [RouterModule]
 })
 export class AppComponent {}
