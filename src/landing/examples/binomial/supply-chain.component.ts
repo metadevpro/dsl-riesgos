07e5c345af4def570855683e286549b694cce466
juanjo8881 <arenasamoresjuanjo@gmail.com>
2026-06-06 21:13:52 +0200

    Sticky-Panels, examples apart

---
+++ src/landing/examples/binomial/supply-chain.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'risk-example-supply-chain',
+  templateUrl: './supply-chain.component.html',
+  styleUrl: '../example.shared.scss',
+  imports: [RouterModule]
+})
+export class SupplyChainComponent {}
